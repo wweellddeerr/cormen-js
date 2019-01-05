1050e3576ad196487eff0d83681df78fb4d23051
Welder Luz <wweellddeerr@gmail.com>
2019-01-05 16:30:40 -0200

    insertion sort

---
+++ insertion-sort.js
@@ -0,0 +1,17 @@
+const A = [10, 2, 3, 9, 1, 13, 5, 4, 22, 14, 17, 11];
+
+let i, j, key
+
+for (j = 1; j < A.length; j++) {
+    key = A[j];
+    i = j - 1
+
+    while (i >= 0 && A[i] > key) {
+        A[i+1] = A[i]
+        i = i -1
+    }
+
+    A[i+1] = key
+}
+
+console.log(A)
